f78b25ff2b36c4ba6b9aa02e138ef908edf13e87
Vladyslav <frontendvlados@gmail.com>
2022-01-20 23:47:59 +0200

    create repository and push homework

---
+++ script.js
@@ -0,0 +1,11 @@
+// $(".test").css("color", "red");
+// $("h2.test").css("color", "red");
+// $("p + p").css("color", "red")
+// $("li.www ~ li").css("color", "red")
+// $("h2 i").css("color", "red")
+// $("h2 > i").css("color", "red")
+
+// $("h2:not(.test)").css("color", "red");
+// $(":header:not(.test)").css("color", "red");
+// $(":header").next(":header").css("color", "red")
+// $(":header.test").next(":header.test").css("color", "red")
