b6f54f742c133eac94160fc79857e880e716135b
Vladyslav <frontendvlados@gmail.com>
2022-01-21 21:05:20 +0200

    bug fixes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // $("h2.test").css("color", "red");
 // $("p + p").css("color", "red")
 // $("li.www ~ li").css("color", "red")
-// $("h2 i").css("color", "red")
+// $("h2").find("i").css("color", "red")
 // $("h2 > i").css("color", "red")
 
 // $("h2:not(.test)").css("color", "red");
