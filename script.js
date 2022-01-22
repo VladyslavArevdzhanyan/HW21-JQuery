39de135591e3c8f1820d7abddd4163f5aa5ac099
Vladyslav <frontendvlados@gmail.com>
2022-01-22 18:07:59 +0200

    bug fixes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log('5.1 - 5.6')
-// $(".test").css("color", "red");
-// $("h2.test").css("color", "red");
-// $("p + p").css("color", "red")
-// $("li.www ~ li").css("color", "red")
-// $("h2").find("i").css("color", "red")
-// $("h2 > i").css("color", "red")
+// // console.log('5.1 - 5.6')
+$(".test").css("color", "red");
+$("h2.test").css("color", "red");
+$("p + p").css("color", "red")
+$("li.www + li").css("color", "red")
+$("h2").find("i").css("color", "red")
+$("h2 > i").css("color", "red")
 
-console.log('6.1 - 6.4')
-// $("h2:not(.test)").css("color", "red");
-// $(":header:not(.test)").css("color", "red");
-// $(":header").next(":header").css("color", "red")
-// $(":header.test").next(":header.test").css("color", "red")
+// // console.log('6.1 - 6.4')
+$("h2:not(.test)").css("color", "red");
+$(":header:not(.test)").css("color", "red");
+$(":header + :header").css("color", "red")
+$(":header.test + :header.test").css("color", "red")
